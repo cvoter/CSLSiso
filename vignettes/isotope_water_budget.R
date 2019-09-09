7b3c3e1194eafb59c198b50858b61ec4d6528f9f
Carolyn Voter <cvoter@wisc.edu>
2019-09-09 17:08:30 -0500

    Minor updates to isotope_water_budget Rmd file

--- vignettes/isotope_water_budget.R
+++
@@ -1,6 +0,0 @@
-## ----setup, echo=TRUE, warning=FALSE, message=FALSE----------------------
-library(devtools)
-
-## ----session_info, include=TRUE, echo=TRUE, results='markup'-------------
-devtools::session_info()
-
